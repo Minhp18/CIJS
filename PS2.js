a80bde071bc8314820594ee6cfd1ed9bd502890d
Minhp18 <102460850+Minhp18@users.noreply.github.com>
2022-05-09 00:19:46 +0700

    Delete PS2.js

--- PS2.js
+++
@@ -1,9 +0,0 @@
-function alternatingSums(a) {
-    re = [0, 0]
-    for (let i=0; i=i+1; i<a.lenth){
-        if(i%2==0) re[0] = re[0] + a[i];
-        else re[1] = re[1] + a[i];
-    }
-    return re;
-}
-console.log(alternatingSums([60, 40, 55, 75, 64]))
